e0acbd35383c3bc1930e93ffb143e4aa3c243ddf
vladislawg <vladislaw1612@googlemail.com>
2017-05-29 18:23:08 +0300

    add

--- Aufgabe3/Scheduling/FCFS.c
+++ Aufgabe3/Scheduling/FCFS.c
@@ -2,5 +2,25 @@
 
 void schedule_FCFS(const TaskPool *task_pool) {
     // TODO
-    printf("NOT IMPLEMENTED!\n");
+  //  printf("NOT IMPLEMENTED!\n");
+
+    Task *CPU = NULL;
+    int max_task = task_pool -> size;
+    int index;
+
+    while (!allDone(task_pool)) {
+
+      if(index != max_task){
+        CPU = task_pool->task[index];
+      }
+
+        if (execTask(CPU, 1) < 0) {
+            printf("%sERROR:%s No Task selected to be executed.\n", COLOR_RED, COLOR_RESET);
+            break;
+        }
+        index ++;
+    }
+    printf("\n");
+
+    return;
 }
